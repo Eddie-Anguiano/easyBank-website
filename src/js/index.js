7e356fa637ea28aab22827164eec9a1fd50a02de
Eddie Anguiano <eddiesanguiano@gmail.com>
2020-04-10 20:35:02 -0700

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+import * as model from "./model";
+import "../styles/main.scss";
+
+console.log(model.bro("dude"));
+
+const test = e => {
+  console.log("helo");
+  var one = "one";
+};
+
+test();
---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    main: "./src/js/index.js",
+    vendor: "./src/js/vendor.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(svg|jpg|png|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "images"
+          }
+        }
+      }
+    ]
+  }
+};
